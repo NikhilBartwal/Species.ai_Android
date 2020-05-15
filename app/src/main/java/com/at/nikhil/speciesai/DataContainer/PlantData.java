7460d0d22788aa89942e1eff608195357a6846c5
NikhilBartwal <nikhilbartwal1234@gmail.com>
2020-05-15 12:04:29 +0530

    Separate classes for each database: plant,animal and bird

---
+++ app/src/main/java/com/at/nikhil/speciesai/DataContainer/PlantData.java
@@ -0,0 +1,55 @@
+package com.at.nikhil.speciesai.DataContainer;
+
+public class PlantData {
+    private String name;
+    private String tag;
+    private String description;
+    private String url;
+    private String taxon;
+    private String genus;
+    private String species;
+    private String scientific_name;
+
+    public PlantData(String name,String tag,String description,String url,String taxon,String genus,String species,String scientific_name){
+        this.name = name;
+        this.tag = tag;
+        this.description = description;
+        this.url = url;
+        this.taxon = taxon;
+        this.genus = genus;
+        this.species = species;
+        this.scientific_name = scientific_name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTaxon() {
+        return taxon;
+    }
+
+    public String getGenus() {
+        return genus;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getScientific_name() {
+        return scientific_name;
+    }
+}
