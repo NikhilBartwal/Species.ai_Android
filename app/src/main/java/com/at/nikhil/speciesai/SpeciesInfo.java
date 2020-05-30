8a64f4b6b4960a0f0dd1ea134942166dafd5e81c
NikhilBartwal <nikhilbartwal1234@gmail.com>
2020-05-30 14:58:06 +0530

    Bug fixes

--- app/src/main/java/com/at/nikhil/speciesai/SpeciesInfo.java
+++ app/src/main/java/com/at/nikhil/speciesai/SpeciesInfo.java
@@ -1,5 +1,6 @@
 package com.at.nikhil.speciesai;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.net.Uri;
@@ -8,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,6 +28,9 @@ public class SpeciesInfo extends AppCompatActivity implements DataCellAdapter.It
     private int type;
     private String table;
     private boolean photoTakenByCamera;
+    private TextView infoTitle;
+    private String url;
+    private Button more_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +41,9 @@ public class SpeciesInfo extends AppCompatActivity implements DataCellAdapter.It
         recyclerView.setLayoutManager(layoutManager);
         dataCells = new ArrayList<DataCell>();
         imageDisplay = findViewById(R.id.imageDisplay);
+        infoTitle = findViewById(R.id.infoTitle);
+        more_button = findViewById(R.id.more_button);
+
         Bundle data = getIntent().getExtras();
         prediction = data.getString("Prediction");
         imageUri = Uri.parse(data.getString("imageURI"));
@@ -57,14 +66,26 @@ public class SpeciesInfo extends AppCompatActivity implements DataCellAdapter.It
         databaseAccess.open();
         ArrayList<SpeciesData> speciesData = databaseAccess.getData(table,prediction);
         for(int i=0;i<speciesData.size();i++){
-            if(speciesData.get(i).getValue() != null)
-                dataCells.add(new DataCell(speciesData.get(i).getKey(),speciesData.get(i).getValue()));
-            else
-                continue;
+            if(speciesData.get(i).getValue() != null) {
+                if (i == 0)
+                    infoTitle.setText(speciesData.get(i).getValue());
+                else if (i == 3)
+                    url = speciesData.get(i).getValue();
+                else
+                    dataCells.add(new DataCell(speciesData.get(i).getKey(), speciesData.get(i).getValue()));
+            }
         }
 
         mAdapter = new DataCellAdapter(this,dataCells);
         recyclerView.setAdapter(mAdapter);
+
+        more_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url));
+               startActivity(intent);
+            }
+        });
     }
 
     @Override
